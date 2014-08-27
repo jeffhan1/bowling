448944dc22d01e966954a1ef9b84c50897952c68
Jeff Han <jeff@sequencefactory.com>
2014-08-27 17:06:17 -0400

    Add players

--- db/migrate/20140827203730_create_players.rb
+++ db/migrate/20140827203730_create_players.rb
@@ -1,7 +1,7 @@
 class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
-      t.string : name
+      t.string :name
       t.string :score
 
       t.timestamps
