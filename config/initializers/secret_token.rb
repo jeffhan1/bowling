702d213c7b3d0a7f705294ad794de8de00839432
Jeff Han <jeff@sequencefactory.com>
2014-08-27 15:20:13 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bowling::Application.config.secret_key_base = 'c4250e0e895fa73a2460cb17dee9a633205cb2946e832c584f20137781b70fe95f358d2c3a6b2fc9bba62648b32a9f1de269ac36037d179b8a0ee7e000f6161a'
